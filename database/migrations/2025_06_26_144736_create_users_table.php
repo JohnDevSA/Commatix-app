a423d5bff0403e1c835d445ded648e36cdb1bb7e
Nkidi John Nkogatse <nkiditoday@gmail.com>
2025-06-26 17:14:23 +0200

    Created DocumentTypes and Tenants resources with --generate flag

--- database/migrations/2025_06_26_144736_create_users_table.php
+++ database/migrations/2025_06_26_144736_create_users_table.php
@@ -16,12 +16,27 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->foreignId('user_type_id');
-            $table->foreignId('tenant_id');
+            $table->foreignId('user_type_id')->default('1');
+            $table->foreignId('tenant_id')->default('1');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->foreignId('division_id')->nullable();
             $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
